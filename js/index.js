70303a3b59a1f24d5a358cd67154e61b62774cfe
annika02 <at621504@gmail.com>
2024-09-23 20:20:37 +0600

    some change

--- js/index.js
+++ js/index.js
@@ -2,29 +2,30 @@ function handleDonation(inputId, balanceId) {
     const addDonation = document.getElementById(inputId).value;
     const addDonationNumber = parseFloat(addDonation);
 
-    if (isNaN(addDonationNumber) || addDonationNumber <= 0) {
-        alert("Please enter a valid donation amount.");
-        return;
+    if (addDonationNumber > 0) {
+        const amount = document.getElementById('amount').innerText;
+        const amountNumber = parseFloat(amount);
+
+        const donationAmount = document.getElementById(balanceId).innerText;
+        const donationAmountNumber = parseFloat(donationAmount);
+
+        const remainingBalance = amountNumber - addDonationNumber;
+        if (remainingBalance < 0) {
+            alert("Insufficient funds.");
+            return;
+        }
+        const newDonation = addDonationNumber + donationAmountNumber;
+
+        document.getElementById('amount').innerText = remainingBalance;
+        document.getElementById(balanceId).innerText = newDonation;
+
+        document.getElementById('my_modal_5').showModal();
+        document.getElementById(inputId).value = '';
+    } else {
+        alert("Please enter a valid donation amount!");
     }
-
-    const amount = document.getElementById('amount').innerText;
-    const amountNumber = parseFloat(amount);
-
-    const donationAmount = document.getElementById(balanceId).innerText;
-    const donationAmountNumber = parseFloat(donationAmount);
-
-    const remainingBalance = amountNumber - addDonationNumber;
-    if (remainingBalance < 0) {
-        alert("Insufficient funds.");
-        return;
-    }
-    const newDonation = addDonationNumber + donationAmountNumber;
-
-    document.getElementById('amount').innerText = remainingBalance;
-    document.getElementById(balanceId).innerText = newDonation;
-
-    document.getElementById('my_modal_5').showModal();
 }
+
 
 // Noakhali donation
 document.getElementById('donation-button').addEventListener('click', function() {
@@ -40,3 +41,6 @@ document.getElementById('donation-button1').addEventListener('click', function()
 document.getElementById('donation-button2').addEventListener('click', function() {
     handleDonation('protestInput', 'qouta-donation');
 });
+
+
+
